f5d9b93d3e142be521ff105b940cad2978a17693
Miyamoto-lab <91850801+Miyamoto-lab@users.noreply.github.com>
2021-10-04 15:52:22 +0100

    Create runKallisto4C.sh

---
+++ Kallisto/runKallisto4C.sh
@@ -0,0 +1,12 @@
+kallisto quant -i transcripts2.idx -o 1-15_S15_v2 1-15_S15_R1_001_val_1-cA10-p.fastq 1-15_S15_R2_001_val_2-cT10-p.fastq -b 100
+kallisto quant -i transcripts2.idx -o 1-16_S16_v2 1-16_S16_R1_001_val_1-cA10-p.fastq 1-16_S16_R2_001_val_2-cT10-p.fastq -b 100
+kallisto quant -i transcripts2.idx -o 1-17_S17_v2 1-17_S17_R1_001_val_1-cA10-p.fastq 1-17_S17_R2_001_val_2-cT10-p.fastq -b 100
+kallisto quant -i transcripts2.idx -o 1-18_S18_v2 1-18_S18_R1_001_val_1-cA10-p.fastq 1-18_S18_R2_001_val_2-cT10-p.fastq -b 100
+kallisto quant -i transcripts2.idx -o 1-17_S13_v2 1-7_S13_R1_001_val_1-cA10-p.fastq     1-7_S13_R2_001_val_2-cT10-p.fastq -b 100
+kallisto quant -i transcripts2.idx -o 1-8_S14_v2 1-8_S14_R1_001_val_1-cA10-p.fastq      1-8_S14_R2_001_val_2-cT10-p.fastq -b 100
+kallisto quant -i transcripts2.idx -o D_D1 D_D1_R1_val_1-pw1_10-cN10-pq20w10m20.fastq D_D1_R2_val_2-pw1_10-cN10-pq20w10m20.fastq -b 100
+kallisto quant -i transcripts2.idx -o D_D4 D_D4_R1_val_1-pw1_10-cN10-pq20w10m20.fastq D_D4_R2_val_2-pw1_10-cN10-pq20w10m20.fastq -b 100
+kallisto quant -i transcripts2.idx -o D_D5 D_D5_R1_val_1-pw1_10-cN10-pq20w10m20.fastq D_D5_R2_val_2-pw1_10-cN10-pq20w10m20.fastq -b 100
+kallisto quant -i transcripts2.idx -o DR_DR1 DR_DR1_R1_val_1-pw1_10-cN10-pq20w10m20.fastq DR_DR1_R2_val_2-pw1_10-cN10-pq20w10m20.fastq -b 100
+kallisto quant -i transcripts2.idx -o DR_DR2 DR_DR2_R1_val_1-pw1_10-cN10-pq20w10m20.fastq DR_DR2_R2_val_2-pw1_10-cN10-pq20w10m20.fastq -b 100
+kallisto quant -i transcripts2.idx -o DR_DR3 DR_DR3_R1_val_1-pw1_10-cN10-pq20w10m20.fastq DR_DR3_R2_val_2-pw1_10-cN10-pq20w10m20.fastq -b 100
